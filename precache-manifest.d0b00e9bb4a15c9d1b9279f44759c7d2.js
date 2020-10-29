9ba845aff80279c5209f2f53e3329b7f95740ba1
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-10-29 19:22:52 +0200

    Updates

---
+++ precache-manifest.d0b00e9bb4a15c9d1b9279f44759c7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59aacfffe5b689463c7a5f82dac830c",
+    "url": "/github.io/topcalc/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/github.io/topcalc/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "fcbffd844da2e91daf48",
+    "url": "/github.io/topcalc/static/css/main.941133c1.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/github.io/topcalc/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "fcbffd844da2e91daf48",
+    "url": "/github.io/topcalc/static/js/main.5b8dcac3.chunk.js"
+  },
+  {
+    "revision": "cad66d54071cbc80a4db",
+    "url": "/github.io/topcalc/static/js/runtime~main.2adfc7bf.js"
+  }
+]);
